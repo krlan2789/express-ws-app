09de53199de6d9076da30d606a82f9b40d0ac936
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2023-09-16 18:30:51 +0700

    Update wss.js

--- src/wss.js
+++ src/wss.js
@@ -81,7 +81,7 @@ module.exports = class WSS {
 						wss.broadcast(data);
 					}
 
-					ErrorHandler.insert(data, data);
+					RecordLogHandler.insert(data, data);
 				});
 
 				ws.on("close", function close() {
